5f1e8a0df323cdb666942e903d617e21601767b6
Fabien VAN DEN BERG <fabvdb@hotmail.com>
2022-05-18 16:59:34 +0200

    Exercise complete

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,35 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Flat.create!(
+  name: 'Light & Spacious Garden Flat London',
+  address: '10 Clifton Gardens London W9 1DT',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 75,
+  number_of_guests: 3
+)
+
+Flat.create!(
+  name: 'Stylish House Close to River Thames',
+  address: '5 Queensmill Road London SW6 6JP',
+  description: 'A stylish feel for this house. One double bedroom, large living room, two minutes from the river',
+  price_per_night: 65,
+  number_of_guests: 2
+)
+
+Flat.create!(
+  name: 'St Pancras Clock Tower Guest Suite',
+  address: 'Euston Rd London N1C 4QP',
+  description: 'A great suite next to St Pancras Station',
+  price_per_night: 110,
+  number_of_guests: 2
+)
+
+Flat.create!(
+  name: 'Nice flat in Camberwell',
+  address: '10 Champion Hill, London SE5 8AN',
+  description: 'A comfortable flat at the heart of Camberwell',
+  price_per_night: 56,
+  number_of_guests: 4
+)
